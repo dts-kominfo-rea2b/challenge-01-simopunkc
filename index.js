cde00db70e1f0b8a9eb8be25084540a2b4bc792c
simopunkc <pembela.allah@gmail.com>
2022-05-27 07:00:55 +0700

    submit challenge oleh budiyanto

--- index.js
+++ index.js
@@ -1,11 +1,104 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const uniqueArrayString = (arr) => {
+    let set = new Set();
+    arr.map((v)=>{
+        set.add(v);
+    });
+    return Array.from(set);
+}
+
+const firstUser = {
+    name: "Monica",
+    gender: "Male",
+    age: 17,
+    email: "monica@dingdong.com",
+    favoriteColor: uniqueArrayString([
+        "Yellow",
+        "Pink",
+        "White",
+        "Purple"
+    ]),
+    isHavePet: true,
+    education: [
+        {
+            name: "SD 01",
+            city: "Jakarta",
+            graduate: 2016
+        },
+        {
+            name: "SMP 02",
+            city: "Jakarta",
+            graduate: 2019
+        },
+        {
+            name: "SMA 03",
+            city: "Tangerang"
+        }
+    ],
+    favoriteRestaurant: uniqueArrayString([
+        "Bento",
+        "Sushi",
+        "Pancake",
+        "Eggy",
+        "Tempura",
+        "Bento",
+        "Eggy",
+        "Padang",
+        "Tteok",
+        "Sushi",
+        "Sushi"
+    ])
+};
+const secondUser = {
+    name: "Wendy",
+    gender: "Female",
+    age: 23,
+    email: "wendy@dingdong.com",
+    favoriteColor: uniqueArrayString([
+        "Blue",
+        "Black",
+        "Grey"
+    ]),
+    isHavePet: false,
+    education: [
+        {
+            name: "SD 02",
+            city: "Jakarta",
+            graduate: 2010
+        },
+        {
+            name: "SMP 03",
+            city: "Bogor",
+            graduate: 2013
+        },
+        {
+            name: "SMA 01",
+            city: "Surabaya",
+            graduate: 2016
+        },
+        {
+            name: "Universitas Maju",
+            city: "Tangerang"
+        }
+    ],
+    favoriteRestaurant: uniqueArrayString([
+        "Tempura",
+        "Bento",
+        "Sushi",
+        "Pancake",
+        "Padang",
+        "Katsu",
+        "Geprek",
+        "Pancake",
+        "Eggy"
+    ])
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
